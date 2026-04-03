1fec2467264ec715b4b4522b4c04f71632d90628
Akhil4n <128935538+Akhil4n@users.noreply.github.com>
2026-04-02 21:17:42 -0500

    Add: course-schedule - submission-8

---
+++ Data Structures & Algorithms/course-schedule/submission-8.py
@@ -0,0 +1,27 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = defaultdict(list)
+        for pair in prerequisites:
+            crs, pre = pair[0], pair[1]
+            adj[pre].append(crs)
+
+        seen = set()
+        stackSet = set()
+        
+        def dfs(i):
+            stackSet.add(i)
+            for crs in adj[i]:
+                if crs in stackSet:
+                    return False
+                if not dfs(crs):
+                    return False
+            stackSet.remove(i)
+            seen.add(i)
+            return True
+
+
+        for i in range(numCourses):
+            if i not in seen and not dfs(i):
+                return False
+
+        return True
