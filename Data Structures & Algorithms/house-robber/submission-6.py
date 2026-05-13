344eb0942d6074bbbc68e789d49f757169c91bf5
Akhil4n <128935538+Akhil4n@users.noreply.github.com>
2026-05-13 16:24:22 -0500

    Add: house-robber - submission-6

---
+++ Data Structures & Algorithms/house-robber/submission-6.py
@@ -0,0 +1,11 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        rob1 = rob2 = 0
+
+        for n in nums:
+            temp = rob2
+            if rob1 + n > rob2:
+                rob2 = rob1 + n
+            rob1 = temp
+        
+        return rob2
