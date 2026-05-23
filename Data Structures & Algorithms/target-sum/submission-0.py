45cd60e8ea4c91535e0293cd90187de5eaf8fddc
Akhil4n <128935538+Akhil4n@users.noreply.github.com>
2026-05-22 23:43:23 -0400

    Add: target-sum - submission-0

---
+++ Data Structures & Algorithms/target-sum/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        
+        def dfs(i, curSum):
+            if i == len(nums):
+                if curSum == target:
+                    return 1
+                return 0
+            return dfs(i + 1, curSum + nums[i]) + dfs(i + 1, curSum - nums[i])
+
+        return dfs(0, 0)
