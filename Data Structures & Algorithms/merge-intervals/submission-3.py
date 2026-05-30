abd622c6377dff68bb33380ab951d5dd2bde10c0
Akhil4n <128935538+Akhil4n@users.noreply.github.com>
2026-05-30 18:19:12 -0400

    Add: merge-intervals - submission-3

---
+++ Data Structures & Algorithms/merge-intervals/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        res = []
+        intervals.sort()
+
+        check = None
+
+        for intvl in intervals:
+            if check is None:
+                check = intvl
+                continue
+
+            if intvl[0] <= check[1]:
+                check = [check[0], max(intvl[1], check[1])]
+            else:
+                res.append(check)
+                check = intvl
+        res.append(check)
+        return res
