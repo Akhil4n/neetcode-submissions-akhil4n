78274e4cf5d830e47e18564015112adac001bbb6
Akhil4n <128935538+Akhil4n@users.noreply.github.com>
2026-06-07 19:32:49 -0400

    Add: count-subsequences - submission-1

---
+++ Data Structures & Algorithms/count-subsequences/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        cache = defaultdict(int)
+
+        def dfs(s_ind, t_ind):
+            if t_ind >= len(t):
+                return 1
+            if s_ind >= len(s):
+                return 0
+            if len(s) - s_ind < len(t) - t_ind:
+                return 0
+            if (s_ind, t_ind) in cache:
+                return cache[(s_ind, t_ind)]
+            res = 0
+            if s[s_ind] == t[t_ind]:
+                res += dfs(s_ind + 1, t_ind + 1)
+            res += dfs(s_ind + 1, t_ind)
+            cache[(s_ind, t_ind)] += res
+            return res
+        
+        return dfs(0, 0)
