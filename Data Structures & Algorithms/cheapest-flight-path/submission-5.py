1280d926ba2e69f012a601fe203366e7e6421324
Akhil4n <128935538+Akhil4n@users.noreply.github.com>
2026-06-28 23:21:15 -0400

    Add: cheapest-flight-path - submission-5

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-5.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        adjMap = defaultdict(list)
+        for f in flights:
+            frm, to, time = f
+            adjMap[frm].append((time, to))
+        
+        res = float('inf') 
+        heap = []
+        heapq.heappush(heap, ((0, src, 0)))
+        seen = {}
+        while heap:
+            for i in range(len(heap)):
+                curr = heapq.heappop(heap)
+                if curr[2] > k + 1 or (curr[1] in seen and seen[curr[1]] < curr[2]):
+                    continue
+                currTime = curr[0]
+                if curr[1] == dst:
+                    res = min(res, currTime)
+                    continue
+                seen[curr[1]] = curr[2]
+                for nei in adjMap[curr[1]]:
+                    if nei[1] not in seen:
+                        heapq.heappush(heap,(currTime + nei[0], nei[1], curr[2] + 1))
+            
+        return -1 if res == float('inf') else res
