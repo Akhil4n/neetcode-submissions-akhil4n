9996c24531a07845e2e63eee74b6a8adb8340857
Akhil4n <128935538+Akhil4n@users.noreply.github.com>
2026-07-20 21:13:23 -0500

    Add: pow-x-n - submission-4

---
+++ Data Structures & Algorithms/pow-x-n/submission-4.py
@@ -0,0 +1,20 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        cache = {1: x}
+
+        def mult(n):
+            if n == 0:
+                return 1
+            
+            if n in cache:
+                return cache[n]
+            split = n // 2
+            if n % 2 == 0:
+                return mult(split) * mult(split)
+            else:
+                return mult(split) * mult(split) * mult(1)
+
+        res = mult(abs(n))
+        if n < 0:
+            return 1 / res
+        return res
