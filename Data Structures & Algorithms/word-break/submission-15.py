55dc949bd5fb849d500f8b224ee752b10203f7a3
Akhil4n <128935538+Akhil4n@users.noreply.github.com>
2026-08-24 22:51:36 -0500

    Add: word-break - submission-15

---
+++ Data Structures & Algorithms/word-break/submission-15.py
@@ -0,0 +1,23 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        cache = {}
+        wordDict = set(wordDict)
+
+        def dfs(start, end):
+            if end >= len(s):
+                return s[start:end] in wordDict
+            if start in cache:
+                return cache[start]
+
+            curr = s[start: end + 1]
+            res = False
+            if curr in wordDict:
+                res = dfs(end + 1, end + 1)
+            if not res:
+                res = res or dfs(start, end + 1)
+            cache[start] = res
+            return res
+
+        res = dfs(0, 0)
+        return res
+
